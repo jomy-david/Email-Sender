cef0c7233457cb8edc656d1ac7c8e8ec7c71258a
Asura <jomydkizhakeyil@gmail.com>
2023-07-29 18:00:22 +0530

    First Test Passed
    First working code with Static Content

---
+++ main.py
@@ -0,0 +1,37 @@
+from email.message import EmailMessage
+from password import password
+import ssl,smtplib
+
+
+# Sender and Paswword
+
+mail_id = 'jomypythonprojects@gmail.com'
+mail_pass = password
+
+
+# Recievers
+
+mail_reciever = 'jomy1998@gmail.com'
+
+# Content
+
+subject = 'Test Code'
+body = 'This is just a test'
+
+# content convert to EmailMessage Instance
+
+mail = EmailMessage()
+mail['From'] = mail_id
+mail['To'] = mail_reciever
+mail['Subject'] = subject
+mail.set_content(body)
+
+# Create SSL
+
+context = ssl.create_default_context()
+
+# Script to Login and send Message
+
+with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as sm:
+    sm.login(mail_id,mail_pass)
+    sm.sendmail(mail_id,mail_reciever,mail.as_string())
